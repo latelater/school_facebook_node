00f5754f7f4ee27d90e2db556e8a0ac32ecadc3d
latelater <903430068@qq.com>
2017-09-06 12:17:09 +0800

    链接mongodb

---
+++ README.md
@@ -0,0 +1,12 @@
+# school_facebook 项目后端
+
+基于**Node.js** 、 **MongoDB**、 **Express** 、 **ES6**
+
+执行命令
+
+`npm start`
+
+----
+
+接口列表说明：
+
--- app.js
+++ app.js
@@ -1,33 +1,74 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let multer = require('multer'); // v1.0.5
+let upload = multer(); // for parsing multipart/form-dataire('multer');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let session = require('express-session');
+let mongoStore = require('connect-mongo')(session);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+let index = require('./routes/index');
+let users = require('./routes/users');
 
-var app = express();
+let app = express();
+let options = {
+	server: {
+		socketOptions: {
+			keepAlive: 300000, connectTimeoutMS: 30000
+		}
+	},
+	replset: {
+		socketOptions: {
+			keepAlive: 300000, connectTimeoutMS: 30000
+		}
+	}
+};
+
+// 连接mongodb
+mongoose.connect('mongodb://127.0.0.1/school_facebook_node', options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, '链接错误'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', './images/favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/personal_blog_node', upload.array(), function (req, res, next) {
+  console.log(req.body);
+  res.json(req.body);
+});
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use(session({
+    secret: 'personal_blog_node',
+    store: new mongoStore({
+        url: 'mongodb://127.0.0.1/personal_blog_node',
+        collection: 'session'
+    }),
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(express.static('public'));
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
