ee6a73f533bb146df5e32647f45e8a1607c0217f
tingyugetc <tingyugetc@tingyugetcdeMacBook-Pro.local>
2017-09-07 16:33:38 +0800

    app

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-let multer = require('multer'); // v1.0.5
 // let upload = multer(); // for parsing multipart/form-dataire('multer');
 let mongoose = require('mongoose');
 let cors = require('cors');
@@ -15,19 +14,6 @@ let index = require('./routes/index');
 let users = require('./routes/users');
 
 let app = express();
-
-// var uploads = multer({ dest: 'uploads/  ' })
-
-// var cpUpload = uploads.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
-// app.post('/personal_blog_node', cpUpload, function (req, res, next) {
-//   // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files 
-//   // 
-//   // e.g. 
-//   //  req.files['avatar'][0] -> File 
-//   //  req.files['gallery'] -> Array 
-//   // 
-//   // req.body will contain the text fields, if there were any 
-// })
 
 let options = {
 	server: {
@@ -51,28 +37,12 @@ db.on('error', console.error.bind(console, '链接错误'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-var storge = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-        var fileformat = (file.originalname).split('.');
-        cb(null, file.fieldname+'-'+Date.now()+'.'+fileformat[fileformat.length-1]);
-    }
-})
-
-var upload = multer({storage: storge})
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', './images/favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/personal_blog_node', upload.array(), function (req, res, next) {
-  console.log(req.body);
-  // console.log(req.file);  
-  res.json(req.body);
-});
+app.use(bodyParser.urlencoded({ uploadDir: './uploads/', extended: false }));
+app.use(upload({dest: './uploads/'}).any());
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
